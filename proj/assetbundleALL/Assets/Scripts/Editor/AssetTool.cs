0c67ec01e6bc514d3adaa49f7aa6e13815682e73
callee <heweitykc@163.com>
2015-07-19 13:20:05 +0800

    在线下载资源

---
+++ proj/assetbundleALL/Assets/Scripts/Editor/AssetTool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AssetTool : EditorWindow {
+    [MenuItem("Window/My Window")]
+    static void Init()
+    {        
+        AssetTool window = (AssetTool)EditorWindow.GetWindow(typeof(AssetTool));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        if (GUILayout.Button("生成")) {
+            BuildPipeline.BuildAssetBundles(@"E:\codejam\loadertest\server_assets\webroot",
+                BuildAssetBundleOptions.UncompressedAssetBundle,BuildTarget.Android);
+        }
+    }
+}
---
+++ proj/assetbundleALL/Assets/Scripts/LoadTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+//
+public class LoadTest : MonoBehaviour {
+    public Text lb;
+    string filename = "aliceast";
+
+	void Start () {
+        StartCoroutine(LoadFiles());        
+	}
+
+    IEnumerator LoadFiles()
+    {
+        string url = "http://asts.aliapp.com/" + filename;
+        print("load " + url);
+        lb.text = url + "\r\t";
+        WWW www = new WWW(url);
+        yield return www;
+
+        byte[] data = www.bytes;
+        string filePath = Application.persistentDataPath + "/" + filename;
+        print(" . " + filePath);
+        FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate);
+        lb.text += filePath + "\r\t";
+        stream.Write(data, 0, data.Length);
+        stream.Flush();
+        stream.Close();
+        www.Dispose();
+        gameStart();
+    }
+
+    void gameStart()
+    {
+        AssetBundle ast = AssetBundle.CreateFromFile(Application.persistentDataPath + "/" + filename);
+        GameObject obj = ast.LoadAsset<GameObject>("AliceAST");
+        Instantiate(obj);
+    }	
+}

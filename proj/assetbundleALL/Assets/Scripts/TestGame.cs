72a0e8a806cc6f5175dce409c86000d593f9dbe7
callee <heweitykc@163.com>
2015-07-24 18:02:12 +0800

    测试场景

--- proj/assetbundleALL/Assets/Scripts/TestGame.cs
+++ proj/assetbundleALL/Assets/Scripts/TestGame.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Net;
+using System.Net.Sockets;
 
 public class TestGame : MonoBehaviour {
     public Transform ball;
+    Socket _sock;
 
 	// Use this for initialization
 	void Start () {
-	
+        IPHostEntry hostEntry = Dns.GetHostEntry("www.baidu.com");
+        foreach (IPAddress ip in hostEntry.AddressList) {
+            IPEndPoint iep = new IPEndPoint(ip, 80);
+            _sock = new Socket(iep.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+            _sock.Connect(iep);            
+        }
+        Application.targetFrameRate = 20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (_sock.Connected) {
+            print("connected!!!!");
+        }
 	}
 
     void OnGUI()

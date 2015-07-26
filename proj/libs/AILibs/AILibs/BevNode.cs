6a52a3a80a14b0b4f2e2626724391da5f2001579
callee <heweitykc@163.com>
2015-07-26 12:14:15 +0800

    1

---
+++ proj/libs/AILibs/AILibs/BevNode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AILibs
+{
+    class BevNode
+    {        
+        public static const int BRS_EXECUTING = 0;
+        public static const int BRS_FINISH = 1;
+        public static const int BRS_ERROR_TRANSITION = -1;
+        public static const int MAX_CHILDREN = 16;
+
+        protected String _debugName;
+        protected BevNodePrecondition _precondition;
+        protected List<BevNode> _children;
+        protected BevNode _parent;
+        
+        public string debugName{
+            get {
+                return _debugName;
+            }
+        }        
+
+        public BevNode(string debugName = null)
+        {
+            if (String.IsNullOrEmpty(debugName)) {
+                _debugName = "";
+            } else {
+                _debugName = debugName;
+            }
+        }
+
+        public BevNode addChild(BevNode node)
+        {
+            if (!_children)
+                _children = new List<BevNode>();
+            if (_children.Count == MAX_CHILDREN)
+                throw new Exception(this + "overflow, max children number is " + MAX_CHILDREN);
+
+            _children.Add(node);
+            node._parent = this;
+            return this;
+        }
+
+        public BevNode AddChildAt(BevNode node, int index)
+        {
+            this.addChild(node);
+            if (index < 0)
+                index = 0;
+            else if (index > _children.Count - 1)
+                index = _children.Count;
+
+            for (int i = _children.Count - 1; i > index; --i) { 
+                _children[i] = _children[i-1];
+            }
+
+            _children[index] = node;
+
+            return this;
+        }
+
+        public BevNode setPrecondition(BevNodePrecondition precondition)
+        {
+            _precondition = precondition;
+            return this;
+        }
+
+
+    }
+}
---
+++ proj/libs/AILibs/AILibs/BevNodePrecondition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AILibs
+{
+    class BevNodePrecondition
+    {
+        public BevNodePrecondition()
+        {
+ 
+        }
+
+        public bool evaluate(BevNodeInputParam input)
+        {
+            throw new Exception("This is an abstract method. You need to implement yourself.");
+        }
+    }
+}

5cfd1d6e63fb506e1aa8721a8664e916fc428bad
callee <heweitykc@163.com>
2015-09-17 11:12:28 +0800

    tcp

---
+++ proj/assetbundleALL/Assets/GI/RotateAeound.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAeound : MonoBehaviour {
+    public Transform target;
+    public float angle = 2f;
+    public GameObject prefab0;
+
+    public Camera global;
+    public Camera local;
+
+    void Start () {
+        if (global == null) return;
+        global.gameObject.SetActive(true);
+        local.gameObject.SetActive(false);
+    }
+	
+	
+	void Update () {
+        transform.RotateAround(target.position,  target.up, angle);
+	}
+
+    void OnGUI()
+    {
+        if (transform.GetComponent<RotateAeound>().global == null) return;
+        if (GUI.Button(new Rect(200f, 100f, 200, 55), "增加"))
+        {
+            var newb = Instantiate(prefab0);
+            newb.transform.position = transform.position +
+                new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+            newb.GetComponent<RotateAeound>().global = null;
+            newb.GetComponent<RotateAeound>().local = null;
+        }
+        if (GUI.Button(new Rect(200f, 200f, 200, 55), "camera"))
+        {
+            global.gameObject.SetActive(!global.gameObject.activeSelf);
+            local.gameObject.SetActive(!global.gameObject.activeSelf);
+        }
+    }
+}
---
+++ proj/assetbundleALL/Assets/network/netdemo.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+public class netdemo : MonoBehaviour {
+    TcpClient client;
+	// Use this for initialization
+	void Start () {
+        client = new TcpClient();        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(client.Available);
+	}
+
+    void OnGUI()
+    {
+        if (!client.Connected)
+        {
+            if (GUI.Button(new Rect(200, 200, 200, 50), "连接"))
+            {
+                client.Connect("127.0.0.1", 9090);
+            }
+        }
+        else {
+            if (GUI.Button(new Rect(200, 200, 200, 50), "发送"))
+            {
+                byte[] data = System.Text.Encoding.ASCII.GetBytes("hello,world!");
+                client.GetStream().Write(data,0,data.Length);
+            }
+            GUI.Label(new Rect(200, 300, 200, 50), "len=" + client.Available.ToString());
+        }
+
+        
+    }
+
+    void OnDestroy()
+    {
+        client.Close();
+    }
+}

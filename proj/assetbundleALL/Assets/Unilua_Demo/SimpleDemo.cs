2da0b0f929cf791fc8d9f2b36ea9b5cf6c5d6a85
callee <heweitykc@163.com>
2015-09-30 14:15:09 +0800

    unilua
    unilua demo

---
+++ proj/assetbundleALL/Assets/Unilua_Demo/SimpleDemo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniLua;
+
+public class SimpleDemo : MonoBehaviour {
+    private ILuaState Lua;
+    public string LuaScriptFile = "framework/main.lua";
+    public string luastr = "print('do it...')";
+    
+    void Start () {
+        Lua = LuaAPI.NewState();
+        Lua.L_OpenLibs();
+        var status = Lua.L_DoString(luastr);
+        Debug.Log(status);
+    }
+		
+	void Update () {
+	
+	}
+}

2934dafd059454377db9240b835094f7beef9909
callee <heweitykc@163.com>
2015-10-16 18:49:02 +0800

    1

--- proj/assetbundleALL/Assets/network/server/demo2.go
+++ proj/assetbundleALL/Assets/network/server/demo2.go
@@ -6,17 +6,27 @@ import(
 	"sync"
 )
 
+var once sync.Once
 var l sync.Mutex
 var a string
+var b int
 
 func f(){
+	 once.Do(testdo)
 	 a = "hello,world"
 	 l.Unlock()
+}
+
+func testdo(){
+	b++
+	print("testdo\n")
 }
 
 func main(){
 	l.Lock()
 	go f()
 	l.Lock()
-	print(a)
+	go f()	
+	print(a+"\n")
+	print(b)
 }

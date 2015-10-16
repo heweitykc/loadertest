d7f0226171137f4f9266e75491868d381221cb70
callee <heweitykc@163.com>
2015-10-16 11:34:37 +0800

    SetReplacementShader

---
+++ proj/assetbundleALL/Assets/ReplacementShader/ReplacementShader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplacementShader : MonoBehaviour {
+    public Camera _camera;
+    public Shader _shader;
+    public string replaceTag;
+
+	void Awake () {
+        _camera.SetReplacementShader(_shader, replaceTag);
+    }
+	
+	
+	void Update () {
+	
+	}
+}

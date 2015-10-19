7cc45c1d69de8a7bfe6a6e60bf33485ff8d6f5f5
callee <heweitykc@163.com>
2015-10-19 18:30:03 +0800

    1

--- proj/assetbundleALL/Assets/ugui/ugui.cs
+++ proj/assetbundleALL/Assets/ugui/ugui.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class ugui : MonoBehaviour {
+    public GameObject UI3D;
+    public GameObject UI2D;
 
-	// Use this for initialization
-	void Start () {
+    void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(GUI.Toggle())
-	}
+
+    bool ishow;	
+	void OnGUI () {
+        ishow = GUI.Toggle(new Rect(20, 20, 100, 30), ishow, ishow?"显示界面":"隐藏界面");
+        UI3D.SetActive(ishow);
+        UI2D.SetActive(ishow);
+    }
 }
